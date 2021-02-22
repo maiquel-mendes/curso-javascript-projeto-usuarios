c98a3cd7588c894357ec3fb816af314b9e568fde
maiquel-mendes <maiquelbsb@gmail.com>
2021-02-21 21:38:33 -0300

    update

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-var name = document.querySelector("#exempleInputName");
-var gender = document.querySelectorAll("#form-user-create [name-gender]:checked");
-var birth = document.querySelector("#exempleInputBirth");
-var country = document.querySelector("#exempleInputCountry");
-var email = document.querySelector("#exempleInputEmail");
-var password = document.querySelector("#exempleInputPassword");
-var photo = document.querySelector("#exempleInputFile");
-var admin = document.querySelector("#exempleInputAdmin");
+let userController = new UserController('form-user-create', 'form-user-update', 'table-users');
 
 
-var fields = document.querySelectorAll('#form-user-create [name]');
 
-fields.forEach(function(field, index){
 
-    if (field.name == 'gender') {
 
-        if (field.checked){
 
-            console.log('sim', field)
-        }
-        
-    } else {
 
-        console.log('nao')
-        
-    }
 
-    //console.log(field.name)
-})
+
+
+
---
+++ models/User.js
@@ -0,0 +1,118 @@
+class User {
+
+    constructor(name, gender, email, birth, country, password, photo, admin){
+
+        this._id;
+        this._name = name;
+        this._gender = gender;
+        this._email = email;
+        this._birth = birth;
+        this._country = country;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
+        this._register = new Date();
+
+
+    }
+
+    get id(){
+        return this._id;
+    }
+
+    get name(){
+        return this._name;
+    }
+    get gender(){
+        return this._gender;
+    }
+    get email(){
+        return this._email;
+    }
+    get birth(){
+        return this._birth;
+    }
+    get country(){
+        return this._country;
+    }
+    get password(){
+        return this._password;
+    }
+    get photo(){
+        return this._photo;
+    }
+    set photo(value){
+        this._photo = value;
+    }
+    get admin(){
+        return this._admin;
+    }
+    get register(){
+        return this._register;
+    }
+
+    loadFromJSON(json){
+
+        for (let name in json){
+
+            switch (name) {
+                case '_register':
+                    this[name] = new Date(json[name]) ;
+                    break;
+            
+                default:
+                    this[name] = json[name];
+                    
+            }
+        }
+    }
+
+    getNewID(){
+
+        if (!window.id) window.id = 0;
+
+        id++;
+
+        return id;
+    }
+
+    save(){
+
+        let users = User.getUsersStorage();
+
+        if (this.id > 0) {
+
+            users.map(u=>{ 
+                
+                if (u._id == this.id){
+                    
+                   Object.assign(u, this);
+
+                }});
+            
+            
+        }else{
+
+            this._id = this.getNewID();
+
+            users.push(this);
+    
+        }
+        
+        localStorage.setItem("users", JSON.stringify(users))
+    }
+
+    static getUsersStorage(){
+        
+        let users = [];
+
+        if (localStorage.getItem("users")) {
+
+            users = JSON.parse(localStorage.getItem("users"))
+        }
+
+        return users;
+
+    }
+
+}
